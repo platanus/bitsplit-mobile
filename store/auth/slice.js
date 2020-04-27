d260c616039c3e89f7cab63767747f231f9adf6f
Diego Silva <disilva2@uc.cl>
2020-04-27 11:34:00 -0400

    fix: deleting auto login feature from this branch

--- store/auth/slice.js
+++ store/auth/slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { AsyncStorage } from 'react-native';
 
 const slice = createSlice({
   name: 'auth',
@@ -17,7 +16,6 @@ const slice = createSlice({
       state.loading = false;
       state.token = action.payload.authentication_token;
       state.user = action.payload;
-      saveDataToStorage(state.token, state.user);
       if (action.payload.api_key) state.apiKey = action.payload.api_key;
     },
     loginRejected(state, action) {
@@ -27,16 +25,6 @@ const slice = createSlice({
   },
 
 });
-
-const saveDataToStorage = (token, user) => {
-  AsyncStorage.setItem(
-    'userData',
-    JSON.stringify({
-      token,
-      user,
-    }),
-  );
-};
 
 export default slice;
 
