67f15c72750da6183d7917ff0a0d867d671476f1
Ignacio Castañeda <icastanedaw@gmail.com>
2020-05-03 20:06:17 -0400

    chore(library): add moment.js

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import MenuNavigator from './navigation/MenuNavigator';
 import { store, runSagas, persistor } from './store';
+import moment from 'moment';
+import 'moment/locale/es';
+
+moment.locale('es');
 
 runSagas();
 
