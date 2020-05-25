2966daec47a251181316312f3a451c682a1eaf53
isavega <igvega@uc.cl>
2020-05-24 21:13:15 -0400

    Update package.json

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import MenuNavigator from './navigation/MenuNavigator';
-import { store, runSagas, persistor } from './store';
 import moment from 'moment';
 import 'moment/locale/es';
+import MenuNavigator from './navigation/MenuNavigator';
+import { store, runSagas, persistor } from './store';
 
 moment.locale('es');
 
