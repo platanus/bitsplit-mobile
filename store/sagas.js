ba253b7f3250c5f32fdc39df48bb22ffb0cc056d
Ignacio Castañeda <icastanedaw@gmail.com>
2020-06-08 15:37:57 -0400

    feat(splitwise): add splitwise saga

--- store/sagas.js
+++ store/sagas.js
@@ -2,9 +2,10 @@ import { spawn, all, call } from 'redux-saga/effects';
 import authSaga from './auth/saga';
 import budaSaga from './buda/saga';
 import firebaseSaga from './firebase/saga';
+import splitwiseSaga from './splitwise/saga';
 
 export default function* rootSaga() {
-  const sagas = [authSaga, budaSaga, firebaseSaga];
+  const sagas = [authSaga, budaSaga, firebaseSaga, splitwiseSaga];
 
   yield all(
     sagas.map(saga =>
---
+++ store/splitwise/saga.js
@@ -0,0 +1,24 @@
+/* eslint-disable max-statements */
+import { call, put, takeLatest, select } from 'redux-saga/effects';
+import { actions as splitwiseActions } from './slice';
+import { SPLITWISE_GET_DEBTS } from '../types';
+import api from '../../utils/api';
+
+function* getSplitwiseDebts() {
+  yield put(splitwiseActions.start());
+  try {
+    const {
+      data: {
+        data: { attributes },
+      },
+    } = yield call(api.splitwiseDebts);
+    yield put(splitwiseActions.setDebts(attributes));
+  } catch (err) {
+    yield put(splitwiseActions.setError(err));
+  }
+  yield put(splitwiseActions.finish());
+}
+
+export default function* splitwiseSaga() {
+  yield takeLatest(SPLITWISE_GET_DEBTS, getSplitwiseDebts);
+}
