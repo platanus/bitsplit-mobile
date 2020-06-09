801769dfeb2099b3f918effa513329719f4e8b99
Diego Silva <disilva2@uc.cl>
2020-06-09 04:48:13 -0400

    Update Buda Navegation :zap:

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,6 +4,8 @@ import {
   DrawerItemList,
   createDrawerNavigator,
 } from '@react-navigation/drawer';
+import { enableScreens } from 'react-native-screens';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 import { Button, ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
@@ -15,9 +17,27 @@ import PaymentScreen from '../screens/PaymentScreen/PaymentScreen';
 import PaymentHistoryScreen from '../screens/PaymentHistoryScreen/PaymentHistoryScreen';
 import SplitwiseDebtsScreen from '../screens/SplitwiseDebtsScreen';
 import NotificationScreen from '../screens/Notifications/NotificationScreen';
+import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
+
 import { LOGOUT_REQUEST } from '../store/types';
 
 const Drawer = createDrawerNavigator();
+
+enableScreens();
+const Stack = createNativeStackNavigator();
+
+function profileStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name='Settings'
+        component={ProfileScreen}
+      />
+      <Stack.Screen name='Buda' component={BudaAuthScreen} />
+    </Stack.Navigator>
+  );
+}
 
 const Navigation = () => {
   const token = useSelector(state => state.auth.token);
@@ -33,7 +53,6 @@ const Navigation = () => {
         {token ? (
           <>
             <Drawer.Screen name='Inicio' component={HomeScreen} />
-            <Drawer.Screen name='Buda' component={BudaAuthScreen} />
             <Drawer.Screen name='Pagar' component={PaymentScreen} />
             <Drawer.Screen
               name='Historial de Pagos'
@@ -44,6 +63,7 @@ const Navigation = () => {
               name='Notificaciones'
               component={NotificationScreen}
             />
+            <Drawer.Screen name='Perfil' component={profileStack} />
           </>
         ) : (
           <Drawer.Screen name='Authentication' component={AuthScreen} />
