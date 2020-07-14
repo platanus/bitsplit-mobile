b599911c38e1705682bbcc861b2d10cfcf2046be
Diego Silva <disilva2@uc.cl>
2020-07-14 16:47:45 -0400

    add api call :call:

--- utils/api/transactions/index.js
+++ utils/api/transactions/index.js
@@ -9,9 +9,16 @@ function transactionsHistory() {
   return authedAxios.getInstance().get(`${env.url}/api/v2/transactions`);
 }
 
+function userExists(email) {
+  return authedAxios
+    .getInstance()
+    .post(`${env.url}/api/v2/user_exists`, { email });
+}
+
 const transactionsApi = {
   getBalances,
   transactionsHistory,
+  userExists,
 };
 
 export default transactionsApi;
